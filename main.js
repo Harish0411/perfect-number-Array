e446301cd39986e549035b68da547c4e8e806a5b
Harish0411 <hariharanramasamy98@gmail.com>
2023-02-28 19:27:31 +0530

    return perfect number from array

---
+++ main.js
@@ -0,0 +1,18 @@
+let array = [6,34,67,28,89,496,1234,1000];
+let number;
+let perfect =[];
+for(i=0; i<array.length; i++){
+    number = array[i];
+	 var m=0;
+	for(j=1; j<=number/2; j++){
+	     if(number%j == 0){
+		     m+=j;
+		 }
+	 }
+	 if(m === number && m !== 0){
+	     perfect.push(number);
+	 }
+ }
+ 
+ console.log(array);
+ console.log(perfect);
